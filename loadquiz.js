57799f00ff2e216f680a2972b162eb72c8e15f7d
SkyMomentum <rsdrew@gmail.com>
2014-11-24 18:18:56 -0500

    Quick axaj grab of the raw quiz datafile.

---
+++ loadquiz.js
@@ -0,0 +1,16 @@
+var RadioQuiz = RadioQuiz || {};
+
+RadioQuiz.rawQuiz = ""
+
+RadioQuiz.getTest = function () {
+    var testAjax = $.ajax( {
+        url: "amat_basic_quest_delim.txt",
+        dataType: "text",
+        beforeSend: function() {},
+        success: function(data, textStatus, jqXHR) { RadioQuiz.rawQuiz = data;},
+        error: function() { alert("Failed to load quiz datafile."); },
+        complete: function() {}
+    });
+
+    return testAjax.responseText;
+}
