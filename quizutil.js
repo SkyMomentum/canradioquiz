b28db50246d6d0bafb3c1a7506d958c3430916eb
SkyMomentum <rsdrew@gmail.com>
2014-11-26 02:44:03 -0500

    Put it away!

--- quizutil.js
+++ quizutil.js
@@ -4,6 +4,20 @@ RadioQuiz.clearQuestionUI = function () {
     $( '.questionanswer' ).empty();
 };
 
-RadioQuiz.displayQuestion = function ( q )  {
-
+RadioQuiz.displayQuestion = function ( question, language )  {
+    var offset =  0;
+    var selector = new String;
+    var questionStr = new String;
+    
+    if (language == "french") {
+        offset = 5;
+    } else {
+        offset = 0;
+    }
+    
+    for( var i = 1; i < 5; i++) {
+        questionStr = question[i];
+        selector = "#" + i.toString();
+        $( selector ).append( questionStr );
+    }
 };
